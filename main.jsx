992c3365238139e6853f61f97b6b62aee35682da
BasilwMotion <basilwmotion@gmail.com>
2026-06-25 21:13:32 +0200

    Add Vite and Vercel deployment configuration

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import JARVIS from './jarvis';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <JARVIS />
+  </React.StrictMode>
+);
